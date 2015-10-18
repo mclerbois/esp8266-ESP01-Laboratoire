15d02ee2fafff5a2c76c6fd3ce40dd32be2ef713
Clerbois Michel <michel.clerbois@condorcet.be>
2015-10-18 10:34:48 +0200

    Signed-off-by: Clerbois Michel <michel.clerbois@condorcet.be>

--- esp8266/echo/echo.ino
+++ esp8266/echo/echo.ino
@@ -2,7 +2,7 @@
   M. Clerbois
   17/10/2015
 
-  echo le message reçu.
+  Actions
   
 */
 
@@ -10,7 +10,18 @@
 
 #include "boards.h"       // déclaration des bornes des cartes
 
+#define LED         GPIO_2
+#define PUSH_BUTTON GPIO_0
+
+#define ON          LOW
+#define OFF         HIGH
+
+
 void setup() {
+  pinMode(LED,OUTPUT);
+  pinMode(PUSH_BUTTON,INPUT);
+  digitalWrite(LED,OFF);
+  
   Serial.begin(115200); // initialize serial com at 115200 baud
   Serial.println("Waiting messages");
  }
@@ -21,6 +32,26 @@ void loop() {
   if (Serial.available()>0) { // data received
     String message=Serial.readString();
     Serial.print("Message received: ");
+    Serial.println(message);
+    
+    if (message.equals("LEDON")) {
+      digitalWrite(LED,ON);
+      Serial.println("Has set led ON");
+      return;
+      }
+      
+    if (message.equals("LEDOFF")) {
+      digitalWrite(LED,OFF);
+      Serial.println("Has set led OFF");
+      return;
+      }
+
+     if (message.equals("BUTTON?")) {
+      Serial.println(digitalRead(PUSH_BUTTON) ? "RELEASED" : "PRESSED");
+      return;
+      }
+      
+    Serial.print("UNKNOW COMMAND: ");
     Serial.println(message);
     }
     
