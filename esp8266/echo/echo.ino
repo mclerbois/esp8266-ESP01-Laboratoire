57cddbadac02b7f3680ff245fa3de4787fad78ed
Clerbois Michel <michel.clerbois@condorcet.be>
2015-10-19 17:14:05 +0200

    update

--- esp8266/echo/echo.ino
+++ esp8266/echo/echo.ino
@@ -58,4 +58,31 @@ void loop() {
 }
 
 
+/*
+  M. Clerbois
+  17/10/2015
+  echo le message reçu.
+  
+*/
+
+#define USER_BOARD      ESP01   // déclaration de la carte utilisée ESP01 ou ESP201
+
+#include "boards.h"       // déclaration des bornes des cartes
+
+void setup() {
+  Serial.begin(115200); // initialize serial com at 115200 baud
+  Serial.println("Waiting messages");
+ }
+
+// the loop function runs over and over again forever
+void loop() {
+ 
+  if (Serial.available()>0) { // data received
+    String message=Serial.readString();
+    Serial.print("Message received: ");
+    Serial.println(message);
+    }
+    
+}
+
 
