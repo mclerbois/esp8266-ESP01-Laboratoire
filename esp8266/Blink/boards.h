319543a74f76d77f52b104f238fd8b0cb2088a08
Clerbois Michel <michel.clerbois@condorcet.be>
2015-10-19 17:03:48 +0200

    update

--- esp8266/Blink/boards.h
+++ esp8266/Blink/boards.h
@@ -1,12 +1,15 @@
-#if defined(__BOARDS_H_)
-#else
+/*
+  Déclaration des bornes utilisables pour deux types de carte ESP8266
+*/
+
+#if !defined(__BOARDS_H_)
 #define     __BOARDS_H_
 
 #if !defined(USER_BOARD)
 #error No Board defined!
 #endif
 
-#if USER_BOARD == ESP01
+#if USER_BOARD == ESP01   // Pour une carte ESP01
 #define GPIO_0     0 // INPUT PULLUP 10K !!! Flash button or switch
 #define GPIO_2     2 // connected to a PULLUP 10K
 #define GPIO_1     1 // OUTPUT connected to RXD of programming device
@@ -15,7 +18,7 @@
 #define RX         3 // INPUT  connected to TXD of communicating device
 #endif
 
-#if USER_BOARD == ESP201
+#if USER_BOARD == ESP201  // Pour une carte ESP201
 #define GPIO_0     0 // INPUT PULLUP 10K !!! Flash button or switch
 #define GPIO_2     2 // connected to a PULLUP 10K
 #define GPIO_1     1 // OUTPUT connected to RXD of programming device
